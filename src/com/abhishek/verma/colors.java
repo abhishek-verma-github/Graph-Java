b2127771d01238541230a107c337b7de2f1dd882
Abhishek Verma <59361086+abhishek-verma-github@users.noreply.github.com>
2021-06-19 12:13:57 +0530

    minor changes

--- src/com/abhishek/verma/colors.java
+++ src/com/abhishek/verma/colors.java
@@ -1,14 +1,11 @@
 package com.abhishek.verma;
 
 public class colors {
-
+    // define constants
     public static final String WHITE = "WHITE";
     public static final String BLACK = "BLACK";
     public static final String GRAY = "GRAY";
     public static final String RED = "RED";
     public static final String GREEN = "GREEN";
-
-    public colors(){}
-
 
 }
